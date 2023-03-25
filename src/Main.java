131d7432ffc89835495d07cacf077c1133e1ad08
lautaro <lautarovillalbafabricio@gmail.com>
2023-03-25 19:51:41 -0300

    primera clase

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 14;
+
+        while (a <= b){
+
+            if ((a % 2)== 0 ) {
+                System.out.println(a+"es par");
+            }
+            else{
+                System.out.println(a+"es impar");
+            }
+            a = a +1;
+        }
+
+    }
+}
